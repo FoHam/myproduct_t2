960ba519d053395bda90ea4eea70bd6dc723eff5
FoHam <spodos37@naver.com>
2023-03-28 17:31:06 +0900

    23-03

---
+++ src/main/java/com/kh/demo/web/ProductControllerCSR.java
@@ -0,0 +1,25 @@
+package com.kh.demo.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/csr")
+public class ProductControllerCSR {
+
+  @GetMapping("/products")
+  public String view(){
+    return "csr/products/view";
+  }
+  @GetMapping("/productsV2")
+  public String view2(){
+    return "csr/products/view2";
+  }
+  @GetMapping("/productsVH")
+  public String view_ham(){
+    return "csr/products/view_ham";
+  }
+}
